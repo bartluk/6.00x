6e758fa5d585d731cb5eda0fba5787da5a23e9df
Felipe Riveroll Aguirre <friveroll@gmail.com>
2012-10-30 00:05:07 -0600

    PSET5

--- Week5/ProblemSet5/ps5_encryption.py
+++ Week5/ProblemSet5/ps5_encryption.py
@@ -11,76 +11,76 @@ WORDLIST_FILENAME = "words.txt"
 # Helper code
 # (you don't need to understand this helper code)
 def loadWords():
-    """
-    Returns a list of valid words. Words are strings of lowercase letters.
-    
-    Depending on the size of the word list, this function may
-    take a while to finish.
-    """
-    print "Loading word list from file..."
-    inFile = open(WORDLIST_FILENAME, 'r')
-    wordList = inFile.read().split()
-    print "  ", len(wordList), "words loaded."
-    return wordList
+	"""
+	Returns a list of valid words. Words are strings of lowercase letters.
+	
+	Depending on the size of the word list, this function may
+	take a while to finish.
+	"""
+	print "Loading word list from file..."
+	inFile = open(WORDLIST_FILENAME, 'r')
+	wordList = inFile.read().split()
+	print "  ", len(wordList), "words loaded."
+	return wordList
 
 def isWord(wordList, word):
-    """
-    Determines if word is a valid word.
+	"""
+	Determines if word is a valid word.
 
-    wordList: list of words in the dictionary.
-    word: a possible word.
-    returns True if word is in wordList.
+	wordList: list of words in the dictionary.
+	word: a possible word.
+	returns True if word is in wordList.
 
-    Example:
-    >>> isWord(wordList, 'bat') returns
-    True
-    >>> isWord(wordList, 'asdf') returns
-    False
-    """
-    word = word.lower()
-    word = word.strip(" !@#$%^&*()-_+={}[]|\\:;'<>?,./\"")
-    return word in wordList
+	Example:
+	>>> isWord(wordList, 'bat') returns
+	True
+	>>> isWord(wordList, 'asdf') returns
+	False
+	"""
+	word = word.lower()
+	word = word.strip(" !@#$%^&*()-_+={}[]|\\:;'<>?,./\"")
+	return word in wordList
 
 def randomWord(wordList):
-    """
-    Returns a random word.
+	"""
+	Returns a random word.
 
-    wordList: list of words  
-    returns: a word from wordList at random
-    """
-    return random.choice(wordList)
+	wordList: list of words  
+	returns: a word from wordList at random
+	"""
+	return random.choice(wordList)
 
 def randomString(wordList, n):
-    """
-    Returns a string containing n random words from wordList
+	"""
+	Returns a string containing n random words from wordList
 
-    wordList: list of words
-    returns: a string of random words separated by spaces.
-    """
-    return " ".join([randomWord(wordList) for _ in range(n)])
+	wordList: list of words
+	returns: a string of random words separated by spaces.
+	"""
+	return " ".join([randomWord(wordList) for _ in range(n)])
 
 def randomScrambled(wordList, n):
-    """
-    Generates a test string by generating an n-word random string
-    and encrypting it with a sequence of random shifts.
+	"""
+	Generates a test string by generating an n-word random string
+	and encrypting it with a sequence of random shifts.
 
-    wordList: list of words
-    n: number of random words to generate and scamble
-    returns: a scrambled string of n random words
+	wordList: list of words
+	n: number of random words to generate and scamble
+	returns: a scrambled string of n random words
 
-    NOTE:
-    This function will ONLY work once you have completed your
-    implementation of applyShifts!
-    """
-    s = randomString(wordList, n) + " "
-    shifts = [(i, random.randint(0, 25)) for i in range(len(s)) if s[i-1] == ' ']
-    return applyShifts(s, shifts)[:-1]
+	NOTE:
+	This function will ONLY work once you have completed your
+	implementation of applyShifts!
+	"""
+	s = randomString(wordList, n) + " "
+	shifts = [(i, random.randint(0, 25)) for i in range(len(s)) if s[i-1] == ' ']
+	return applyShifts(s, shifts)[:-1]
 
 def getStoryString():
-    """
-    Returns a story in encrypted text.
-    """
-    return open("story.txt", "r").read()
+	"""
+	Returns a story in encrypted text.
+	"""
+	return open("story.txt", "r").read()
 
 
 # (end of helper code)
@@ -91,72 +91,93 @@ def getStoryString():
 # Problem 1: Encryption
 #
 def buildCoder(shift):
-    """
-    Returns a dict that can apply a Caesar cipher to a letter.
-    The cipher is defined by the shift value. Ignores non-letter characters
-    like punctuation, numbers and spaces.
+	"""
+	Returns a dict that can apply a Caesar cipher to a letter.
+	The cipher is defined by the shift value. Ignores non-letter characters
+	like punctuation, numbers and spaces.
 
-    shift: 0 <= int < 26
-    returns: dict
-    """
-    ### TODO.
-    
+	shift: 0 <= int < 26
+	returns: dict
+	"""
+	letters_lower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+	
+	letters_1dic = {}
+	letters_2dic = {}
+	i = 0
+	
+	for letter in string.ascii_lowercase:    
+		if i < len(string.ascii_lowercase)-shift: 
+			letters_1dic[letter] = letters_lower[i + shift]
+			letters_2dic[letter.upper()] = letters_lower[i + shift].upper()
+			i += 1 
+		if i >= len(string.ascii_lowercase)-shift:
+			letters_1dic[letter] = letters_lower[i + shift - 27]
+			letters_1dic[letter.upper()] = letters_lower[i + shift - 27].upper()
+			i += 1
+		if i == len(string.ascii_lowercase)+shift-1:
+			break
+	#print i, letter
+	return dict(letters_1dic.items() + letters_2dic.items())
+	
 
 def applyCoder(text, coder):
-    """
-    Applies the coder to the text. Returns the encoded text.
+	"""
+	Applies the coder to the text. Returns the encoded text.
 
-    text: string
-    coder: dict with mappings of characters to shifted characters
-    returns: text after mapping coder chars to original text
-    """
-    ### TODO.
-    
+	text: string
+	coder: dict with mappings of characters to shifted characters
+	returns: text after mapping coder chars to original text
+	"""
+	### TODO.
+	
 
 def applyShift(text, shift):
-    """
-    Given a text, returns a new text Caesar shifted by the given shift
-    offset. Lower case letters should remain lower case, upper case
-    letters should remain upper case, and all other punctuation should
-    stay as it is.
+	"""
+	Given a text, returns a new text Caesar shifted by the given shift
+	offset. Lower case letters should remain lower case, upper case
+	letters should remain upper case, and all other punctuation should
+	stay as it is.
 
-    text: string to apply the shift to
-    shift: amount to shift the text (0 <= int < 26)
-    returns: text after being shifted by specified amount.
-    """
-    ### TODO.
-    ### HINT: This is a wrapper function.
-    
+	text: string to apply the shift to
+	shift: amount to shift the text (0 <= int < 26)
+	returns: text after being shifted by specified amount.
+	"""
+	### TODO.
+	### HINT: This is a wrapper function.
+	
 
 #
 # Problem 2: Decryption
 #
 def findBestShift(wordList, text):
-    """
-    Finds a shift key that can decrypt the encoded text.
+	"""
+	Finds a shift key that can decrypt the encoded text.
 
-    text: string
-    returns: 0 <= int < 26
-    """
-    ### TODO
+	text: string
+	returns: 0 <= int < 26
+	"""
+	### TODO
 
 
 def decryptStory():
-    """
-    Using the methods you created in this problem set,
-    decrypt the story given by the function getStoryString().
-    Use the functions getStoryString and loadWords to get the
-    raw data you need.
+	"""
+	Using the methods you created in this problem set,
+	decrypt the story given by the function getStoryString().
+	Use the functions getStoryString and loadWords to get the
+	raw data you need.
 
-    returns: string - story in plain text
-    """
-    ### TODO.
-    return "Not yet implemented." # Remove this comment when you code the function
+	returns: string - story in plain text
+	"""
+	### TODO.
+	return "Not yet implemented." # Remove this comment when you code the function
 
 #
 # Build data structures used for entire session and run encryption
 #
 
 if __name__ == '__main__':
-    wordList = loadWords()
-    decryptStory()
+	wordList = loadWords()
+	decryptStory()
+
+
+print buildCoder(3)
